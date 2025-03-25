3611afae20362898e09e23a7e12df1ab06a2db8e
Cole Whitehurst <colewhitehurst96@gmail.com>
2025-03-25 13:54:16 -0400

    eod 2

--- src/components/NewPlayerForm.jsx
+++ src/components/NewPlayerForm.jsx
@@ -1,59 +1,82 @@
-import { useState, useEffect } from 'react';
-import { addNewPlayer, fetchAllPlayers } from '../API';
+import { useState, useEffect } from "react";
+import { addNewPlayer, fetchAllPlayers } from "../API";
 
+const NewPlayersForm = ({ players, setPlayers }) => {
+  const [name, setName] = useState("");
+  const [breed, setBreed] = useState("");
+  const [status, setStatus] = useState("field");
+  const [imageUrl, setImageUrl] = useState('');
+  const [error, setError] = useState(null);
 
-const NewPlayersForm = ({players, setPlayers}) => {
-    const [name, setName] = useState("");
-    const [breed, setBreed] = useState("");
-    const [status, setStatus] = useState("field");
-    const [image,setImage] = useState("");
-    const [error, setError] = useState(null);
-
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-            const response = await fetch('https://fsa-puppy-bowl.herokuapp.com/api/2502-FTB-ET-WEB-FT/players', {
-                method: "POST",
-                headers: {"Content-Type": "application/json"},
-                body: JSON.stringify({name, breed, status, image}),
-            });
-            window.location.reload();
-        } catch (error) {
-            setError(error.message);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch(
+        "https://fsa-puppy-bowl.herokuapp.com/api/2502-FTB-ET-WEB-FT/players",
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ name, breed, status, imageUrl }),
         }
-        setName("");
-        setBreed("");
-        setImage("");
+      );
+      window.location.reload();
+    } catch (error) {
+      setError(error.message);
     }
-    
+    setName("");
+    setBreed("");
+    setImageUrl("");
+  };
 
-    return ( 
+  return (
     <>
-       <form className="form" onSubmit={handleSubmit}>
-            <label>
-                Name:{" "}
-                <input value={name} placeholder="Enter Name" onChange={(e) => setName(e.target.value)} required/>
-            </label>
-            
-            <label>
-                {" "}Breed:{" "}
-                <input value={breed} placeholder="Enter Breed" onChange={(e) => setBreed(e.target.value)} required/>
-            </label><br />
+      <form className="form" onSubmit={handleSubmit}>
+        <label>
+          Name:{" "}
+          <input
+            value={name}
+            placeholder="Enter Name"
+            onChange={(e) => setName(e.target.value)}
+            required
+          />
+        </label>
 
-            <label htmlFor="status">{" "}Status:{" "}</label>
-            <select name="status" onChange={((e) => setStatus(e.target.value))}>
-                <option defaultValue value="field">Field</option>
-                <option value="bench">Bench</option>
-            </select>
+        <label>
+          {" "}
+          Breed:{" "}
+          <input
+            value={breed}
+            placeholder="Enter Breed"
+            onChange={(e) => setBreed(e.target.value)}
+            required
+          />
+        </label>
+        <br />
 
-            <label htmlFor="imageUrl">{" "}Image Link:{" "}</label>
-            <input type="text" id="image" placeholder="Enter Link        " onChange={((e) =>setImage(e.target.value))}></input><br />
+        <label htmlFor="status"> Status: </label>
+        <select name="status" onChange={(e) => setStatus(e.target.value)}>
+          <option defaultValue value="field">
+            Field
+          </option>
+          <option value="bench">
+            Bench
+          </option>
+        </select>
 
-            <button type="submit">Submit</button>
-        </form>
+        <label htmlFor="imageUrl"> Image Link: </label>
+        <input
+          type="text"
+          id="imageUrl"
+          value={imageUrl}
+          placeholder="Enter Link"
+          onChange={(e) => setImageUrl(e.target.value)}
+        ></input>
+        <br />
+
+        <button type="submit">Submit</button>
+      </form>
     </>
-     );
-}
- 
+  );
+};
+
 export default NewPlayersForm;
--- src/components/SinglePlayer.jsx
+++ src/components/SinglePlayer.jsx
@@ -1,45 +1,44 @@
-import { useEffect, useState } from 'react';
-import { fetchSinglePlayer } from '../API';
-import { useParams, useNavigate } from 'react-router-dom';
+import { useEffect, useState } from "react";
+import { fetchSinglePlayer } from "../API";
+import { useParams, useNavigate } from "react-router-dom";
 
 const SinglePLayer = () => {
-    const [player, setPlayer] = useState(null);
-    const { id } = useParams();
-    const navigate = useNavigate();
+  const [player, setPlayer] = useState(null);
+  const { id } = useParams();
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        async function selectPlayer() {
-            const APIResponse = await fetchSinglePlayer(id);
-            console.log('single player fetch call', APIResponse)
-           setPlayer(APIResponse)
-        }
-        selectPlayer();
-    }, []);
+  useEffect(() => {
+    async function selectPlayer() {
+      const APIResponse = await fetchSinglePlayer(id);
+    //   console.log("single player fetch call", APIResponse);
+      setPlayer(APIResponse);
+    }
+    selectPlayer();
+  }, []);
 
-    return ( 
-<>
-    <div className="player">
-      {player && (
-        <div className="puppy">
-          <p>
-            <b>Name:</b> {player.name}
-          </p>
-          <p>
-            <b>Breed:</b> {player.breed}
-          </p>
-          <p>
-            <b>Status:</b> {player.status}
-          </p>
-          <img src={player.imageUrl} />
-        </div>
-      )}
-    
-    </div>
-    <button className="back" onClick={() => navigate(-1)}>
-    Back
-    </button>
-</>
-     
-    )}
- 
+  return (
+    <>
+      <div className="player">
+        {player && (
+          <div className="puppy">
+            <p>
+              <b>Name:</b> {player.name}
+            </p>
+            <p>
+              <b>Breed:</b> {player.breed}
+            </p>
+            <p>
+              <b>Status:</b> {player.status}
+            </p>
+            <img src={player.imageUrl} alt="puppy" />
+          </div>
+        )}
+      </div>
+      <button className="back" onClick={() => navigate(-1)}>
+        Back
+      </button>
+    </>
+  );
+};
+
 export default SinglePLayer;
